67e7fdb4191de6d52ccd59cb327ed8a3d9d271f2
Matthias Steinkogler <matthias.steinkogler@microfocus.com>
2017-03-16 12:58:29 +0100

    add license and some code documentation.

--- MMM-updateFromStdOut.js
+++ MMM-updateFromStdOut.js
@@ -1,58 +1,76 @@
+/*
+Provided under the MIT License.
 
-Module.register("MMM-updateFromStdOut",{
-	// Default module config.
-	defaults: {
-	},
-    
-	// Define start sequence.
-	start: function() {
-		Log.info("Starting module: " + this.name);
-		
-		this.loaded = false;
-		this.temperature = '';
-		this.humidity= '';
-		this.battery = '';
-     
-		this.update();
-	},
-    
-	update: function(){
-		this.sendSocketNotification('REQUEST-MMM-updateFromStdOut', this.config);
-	},
+Copyright (c) 2017 Matthias Steinkogler
 
-	getDom: function() {	
-		var wrapper = document.createElement("div");
-		wrapper.className = "bright large light";
-    	
-		if (!this.loaded) {
-			wrapper.innerHTML = "Loading ...";
-			wrapper.className = "dimmed light small";
-			return wrapper;
-		}
-    	
-		var div = document.createElement("div");
-		div.innerHTML = "<span class='bold'>" + this.temperature + "</span>&deg;C&nbsp;";
-		div.innerHTML += "<span class='bold'>" + this.humidity + "</span>%";
-		wrapper.appendChild(div);
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-		return wrapper;
-	},
-    
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-	socketNotificationReceived: function(notification, payload) {
-		console.log("received");
-		if (notification === 'DATA-MMM-updateFromStdOut') {
-			if (payload.temp != "-1000") {
-				this.temperature = payload.temp;
-			}
-			if (payload.humidity != "-1000") {
-				this.humidity = payload.humidity;
-			}
-			if (payload.battery != "exploding") {
-				this.battery = payload.battery;
-			}
-			this.loaded = 1;
-			this.updateDom();
-		}
-	},
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+Module.register("MMM-updateFromStdOut", {
+  // Default module config.
+  defaults: {
+  },
+
+  start: function () {
+    Log.info("Starting module: " + this.name);
+
+    this.loaded = false;
+    this.temperature = '';
+    this.humidity = '';
+    this.battery = '';
+
+    // initialize socket communication with node_helper so it can send us information.    
+    this.sendSocketNotification('REQUEST-MMM-updateFromStdOut', this.config);
+  },
+
+  getDom: function () {
+    var wrapper = document.createElement("div");
+    wrapper.className = "bright large light";
+
+    if (!this.loaded) {
+      wrapper.innerHTML = "Loading...";
+      wrapper.className = "dimmed light small";
+      return wrapper;
+    }
+
+    var div = document.createElement("div");
+    div.innerHTML = "<span class='bold'>" + this.temperature + "</span>&deg;C&nbsp;";
+    div.innerHTML += "<span class='bold'>" + this.humidity + "</span>%";
+    wrapper.appendChild(div);
+
+    return wrapper;
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === 'DATA-MMM-updateFromStdOut') {
+      if (payload.temp != "-1000") {
+        this.temperature = payload.temp;
+      }
+      if (payload.humidity != "-1000") {
+        this.humidity = payload.humidity;
+      }
+      if (payload.battery != "exploding") {
+        this.battery = payload.battery;
+      }
+      this.loaded = 1;
+      
+      this.updateDom();
+    }
+  },
 });
--- node_helper.js
+++ node_helper.js
@@ -1,40 +1,67 @@
+/*
+Provided under the MIT License.
+
+Copyright (c) 2017 Matthias Steinkogler
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 'use strict';
 
 const NodeHelper = require('node_helper');
 const spawn = require('child_process').spawn;
 
 module.exports = NodeHelper.create({
-	start: function() {
-		console.log('MMM-updateFromStdOut helper started ...');
-	},
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === 'REQUEST-MMM-updateFromStdOut') {
-			var self = this;
+  start: function () {
+    console.log('MMM-updateFromStdOut helper started...');
+  },
 
-	                var rtl_433 = spawn('/usr/local/bin/rtl_433', ['-q', '-G', '-s', '1000000'], {
-        	                detached: true
-                	});
-	                rtl_433.stdout.on('data', function (data) {
-        	                try {
-                	                var strData = data.toString();
-	
-        	                        var temp = "-1000";
-                	                var humidity = "-1000";
-                        	        var battery = "exploding";
-	
-        	                        temp = strData.split("temperature ")[1].split(" C / ")[0];
-                	                humidity = strData.split("humidity ")[1].split("%")[0];
-                        	        battery = strData.split("battery ")[1].split(", ")[0];
-	
-        	                        self.sendSocketNotification('DATA-MMM-updateFromStdOut', {
-                	                        temp: temp,
-                        	                humidity: humidity,
-                                	        battery: battery
-	                                });
-        	                } catch(err) {
-                	                console.log(err);
-                        	}
-	                });
-		}
-	}
+  socketNotificationReceived: function (notification, payload) {
+    // we receive this notification upon startup of the module, then we can respond.
+    if (notification === 'REQUEST-MMM-updateFromStdOut') {
+      var self = this;
+
+      var rtl_433 = spawn('/usr/local/bin/rtl_433', ['-q', '-G', '-s', '1000000'], {
+        detached: true
+      });
+
+      rtl_433.stdout.on('data', function (data) {
+        try {
+          var strData = data.toString();
+
+          var temp = "-1000";
+          var humidity = "-1000";
+          var battery = "exploding";
+
+          temp = strData.split("temperature ")[1].split(" C / ")[0];
+          humidity = strData.split("humidity ")[1].split("%")[0];
+          battery = strData.split("battery ")[1].split(", ")[0];
+
+          self.sendSocketNotification('DATA-MMM-updateFromStdOut', {
+            temp: temp,
+            humidity: humidity,
+            battery: battery
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      });
+    }
+  }
 });
